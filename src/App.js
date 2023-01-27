e17b6b54679b8cde286e64a3c45e0197dcf3cd1c
Harish Kumar <harish_kumar@persistent.com>
2023-01-27 14:41:37 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper bg-dark d-flex align-item-center justify-content-center w-100'>
+      <div className='login'>
+        <h2 className='mb-3' align="center">Login</h2>
+        <form className='needs-validation'>
+          <div className='form-group was-validated mb-2'>
+            <label htmlFor='email' className='form-label'>Email Address</label>
+            <input type="email" className='form-control' required></input>
+          </div>
+          <div className='form-group was-validated mb-2'>
+            <label htmlFor='password' className='form-label'>Password</label>
+            <input type="password" className='form-control' required></input>
+          </div>
+          <button type='submit' className='btn btn-success w-100 mt-2'>Sign In</button>
+        </form>
+      </div>
     </div>
   );
 }
